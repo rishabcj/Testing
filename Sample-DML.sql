2c75f6648908b194d2c6a6befbf190edeb0d6849
Rishab Kumar Panneerselvam <Rishabkumar.P@avasoft.com>
2023-09-03 14:49:36 +0530

    adding smaple DML file

---
+++ Sample-DML.sql
@@ -0,0 +1,13 @@
+INSERT INTO configadm.test_temp
+(liquibase_id, liquibase_description)
+VALUES(1, 'Test_1');
+
+
+INSERT INTO configadm.test_temp
+(liquibase_id, liquibase_description)
+VALUES(2, 'Test_2');
+
+
+INSERT INTO configadm.test_temp
+(liquibase_id, liquibase_description)
+VALUES(3, 'Test_2');
